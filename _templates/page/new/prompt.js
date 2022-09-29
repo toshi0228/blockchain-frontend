01be1690695a8d2978a3a0ff0870cf4b443f50fc
toshi0228 <etoshi0228@gmail.com>
2022-09-29 21:13:28 +0900

    rxPoolPage　の大枠作成

--- _templates/page/new/prompt.js
+++ _templates/page/new/prompt.js
@@ -1,17 +1,17 @@
 module.exports = [
-    {
-        message: "ロール名は？",
-        type: 'input',
-        name: 'dir',
-    },
-    {
-        message: "カテゴリー名は？",
-        type: 'input',
-        name: 'category',
-    },
-    {
-        message: 'コンポーネント名は？',
-        type: 'input',
-        name: 'name',
-    },
+  {
+    message: "ロール名は ex) User, Admin, Common？ ※キャメルケース",
+    type: "input",
+    name: "dir",
+  },
+  {
+    message: "カテゴリー名は ex) Auth, MainPage, MyPage？　※キャメルケース",
+    type: "input",
+    name: "category",
+  },
+  {
+    message: "コンポーネント名は ex) Auction, ProfileDetail , Search？　※キャメルケース",
+    type: "input",
+    name: "name",
+  },
 ];
---
+++ src/page/User/MainPage/TransactionPool/TransactionPool.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import { Container } from './style';
+
+
+export interface ITransactionPoolProps {
+
+}
+
+const TransactionPool: React.FC<ITransactionPoolProps> = props => {
+  return (
+    <Container>
+
+    </Container>
+  );
+};
+
+export default TransactionPool;
